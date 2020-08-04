8709fe771100818ad2ad7e383d147cb5b35748ff
Soumya Prakash Nayak <56496307+soumya969@users.noreply.github.com>
2020-08-04 20:01:26 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<meta http-equiv="refresh" content="5" url="http://www.facebook.com"/>
+</head>
+<body>
+	<div class="loginbox">
+		<img src="images/b.jpg" class="users"/>
+		<h1>Login Here!</h1>
+		<form action="chk.php" method="post">
+			<input type="text" placeholder="user id" name="user id"/>
+			<br/>
+			<input type="password" placeholder="password" name="password"/>
+			<br/>
+			<input type="submit" value="Login" />
+			<br/>
+		</form>
+		
+	</div>
+
+</body>
+</html>
